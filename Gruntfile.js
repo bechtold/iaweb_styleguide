f89dfa9f05de26db5620fafd3b0b95cb30f65032
Oskar Bechtold <bechtold@brightsolutions.de>
2015-12-14 20:28:00 +0100

    Added Grunt tasks and options for sass, watch and concurrent.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,13 +23,68 @@ module.exports = function (grunt) {
           future: true
         }
       }
-    }
+    },
 
+
+    // Grunt-sass 
+    sass: {
+      app: {
+        // Takes every file that ends with .scss from the scss 
+        // directory and compile them into the css directory. 
+        // Also changes the extension from .scss into .css. 
+        // Note: file name that begins with _ are ignored automatically
+        files: [{
+          expand: true,
+          cwd: 'scss',
+          src: ['*.scss'],
+          dest: 'css',
+          ext: '.css'
+        }]
+      },
+      options: {
+        sourceMap: false,
+        outputStyle: 'nested',
+        imagePath: "../",
+      }
+    },
+
+    // Grunt-contrib-watch
+    watch: {
+      sass: {
+        // Watches all Sass or Scss files within the scss folder and one level down. 
+        // If you want to watch all scss files instead, use the "**/*" globbing pattern
+        files: ['scss/{,*/}*.{scss,sass}'],
+        // runs the task `sass` whenever any watched file changes 
+        tasks: ['sass']
+      },
+      options: {
+        // Sets livereload to true for livereload to work 
+        // (livereload is not covered in this article)
+        //        livereload: true,
+        //        spawn: false
+      }
+    },
+
+    // run tasks in parallel
+    concurrent: {
+      serve: [
+                'watch',
+                'jekyll:serve'
+            ],
+      options: {
+        logConcurrentOutput: true
+      }
+    },
+    
   });
 
+
   grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-concurrent');
 
   // Default task(s).
-  //grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['concurrent']);
 
 };
